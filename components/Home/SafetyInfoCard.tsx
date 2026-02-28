752054b8e90ca9bb5f9eeabba841a13ca91d5c15
unknown <jjjk0605@naver.com>
2026-02-28 22:31:00 +0900

    feat: SafetyInfoCard가 img를 입력받도록 변경, 외부 링크 클릭 시 새 페이지에서 열리도록 수정

--- components/Home/SafetyInfoCard.tsx
+++ components/Home/SafetyInfoCard.tsx
@@ -2,19 +2,21 @@ import Link from "next/link";
 
 interface SafetyInfoCardProps {
   title: string;
-  description: string;
+  description?: string;
   href: string;
+  img: string;
 }
 
-const SafetyInfoCard = ({ title, description, href }: SafetyInfoCardProps) => {
+const SafetyInfoCard = ({ title, description, img, href }: SafetyInfoCardProps) => {
   return (
-    <Link href={href} className="flex gap-3 justify-center items-center">
-      {/* 이미지(추후 교체) */}
-      <div className="size-19.25 rounded-2xl bg-blue-400" />
+    <Link target="_blank" href={href} className="flex gap-3 justify-center items-center">
+      <img className="size-19.25 rounded-2xl object-cover" src={img} alt={title} />
 
       <div className="flex flex-col gap-1.5 justify-center items-center">
         <h3 className="w-63.5 text-18_B text-gray-950 truncate">{title}</h3>
-        <span className="w-63.5 text-12_M text-gray-600 truncate">{description}</span>
+        {description && (
+          <span className="w-63.5 text-12_M text-gray-600 truncate">{description}</span>
+        )}
       </div>
     </Link>
   );
