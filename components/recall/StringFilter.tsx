054d7b982e9356ec4af128caa0d9086e61ec8635
unknown <jjjk0605@naver.com>
2026-03-01 19:13:31 +0900

    refactor: StringFilter 컴포넌트 내부를 Popover로 변경

--- components/recall/StringFilter.tsx
+++ components/recall/StringFilter.tsx
@@ -1,15 +1,10 @@
 "use client";
 
 import { useState } from "react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger
-} from "../ui/dropdown-menu";
 import { ChevronDown, ChevronUp } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 interface StringFilterProps {
   title: string;
@@ -34,25 +29,33 @@ const StringFilter = ({ title, options, paramKey }: StringFilterProps) => {
   const optionsList = Object.keys(options);
 
   return (
-    <DropdownMenu onOpenChange={setOpen} open={open}>
-      <DropdownMenuTrigger asChild>
+    <Popover onOpenChange={setOpen} open={open}>
+      <PopoverTrigger asChild>
         <button
           className={cn(
-            "flex items-center justify-center gap-1 rounded-[100px] text-14_M outline outline-gray-100 bg-white min-w-20 h-10.25 px-4",
-            open && "bg-gray-950 text-[14px] font-medium text-white "
+            "text-[14px] leading-[100%] font-medium tracking-[-2.5%]",
+            "select-none flex items-center justify-center gap-1 rounded-[100px] outline outline-gray-100 bg-white h-10.25 px-4 cursor-pointer",
+            open && "bg-gray-950 text-white "
           )}
         >
           {filter} {open ? <ChevronUp className="size-4" /> : <ChevronDown className="size-4" />}
         </button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
+      </PopoverTrigger>
+      <PopoverContent className="w-fit min-w-32 p-1 rounded-xl shadow-md flex flex-col items-center">
         {optionsList.map((option) => (
-          <DropdownMenuItem key={option} onClick={() => handleSort(option)}>
+          <button
+            className={cn(
+              "w-full text-left px-2 py-1.5 text-sm rounded-sm hover:bg-gray-100 transition-colors",
+              filter === option && "font-semibold"
+            )}
+            key={option}
+            onClick={() => handleSort(option)}
+          >
             {option}
-          </DropdownMenuItem>
+          </button>
         ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </PopoverContent>
+    </Popover>
   );
 };
 
