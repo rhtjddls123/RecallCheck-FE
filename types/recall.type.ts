f9ea30901526b3e8a15be780051499d8d0e4f37f
unknown <jjjk0605@naver.com>
2026-03-02 23:30:24 +0900

    feat: RecallType 및 RecallPaginationResponse 타입 정의

--- types/recall.type.ts
+++ types/recall.type.ts
@@ -5,3 +5,46 @@ export interface RecentRecallType {
   bsnmNm?: string;
   recallImgUrls: string[];
 }
+
+export interface RecallType {
+  recallSn: string; // 리콜번호 (PK)
+  cntntsId: string; // 메뉴 ID
+  productNm: string; // 제품명
+
+  makr: string | null; // 제조사
+  bsnmNm: string | null; // 사업자명
+  modlNmInfo: string | null; // 모델명 정보
+  stdBrcd: string | null; // 표준 바코드
+  mnfcturPd: string | null; // 제조기간
+  mnfcturNoInfo: string | null; // 제조번호 정보
+  distbTmlmtDe: string | null; // 유통기한
+  prmisnNo: string | null; // 허가번호
+  aditfield13: string | null; // 제품 상세내용
+  shrtcomCn: string | null; // 결함 내용
+  recallSe: string | null; // 리콜 구분
+
+  recallPublictBgnde: string | null; // 리콜 공표 시작일
+  recallPublictEndde: string | null; // 리콜 공표 만료일
+
+  injryCauseResult: string | null; // 위해 원인
+  hrmflGrad: string | null; // 위해성 등급
+  acdntCn: string | null; // 사고 내용
+  cnsmrGhvrTips: string | null; // 소비자 행동 요령
+  trtmntAtpn: string | null; // 취급 주의사항
+
+  recallBgnde: string | null; // 리콜 시작일
+  recallEndde: string | null; // 리콜 종료일
+
+  recallProcssInfo: string | null; // 리콜 절차
+  recallEntrpsInfo: string | null; // 문의처
+
+  infoOriginInstt: string | null; // 정보 출처 기관
+  infoOriginUrl: string | null; // 정보 출처 URL
+  infoCreatInstt: string | null; // 정보 생성 기관
+
+  infoCreatUrl: string[] | null; // 정보 생성 URL (||로 구분)
+  recallImgUrls: string[] | null;
+
+  createdAt: Date;
+  updatedAt: Date;
+}
