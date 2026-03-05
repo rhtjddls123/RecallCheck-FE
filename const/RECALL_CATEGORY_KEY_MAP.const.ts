d1d6edb090dc9501ddecf878bb04dd0ae8bd8414
unknown <jjjk0605@naver.com>
2026-03-05 17:51:08 +0900

    feat: 챗봇 관련 타입 및 상수 선언

--- const/RECALL_CATEGORY_KEY_MAP.const.ts
+++ const/RECALL_CATEGORY_KEY_MAP.const.ts
@@ -14,4 +14,22 @@ export const RECALL_CATEGORY_KEY_MAP = {
   // '해외 리콜':'0501',
 } as const;
 
+export const RECALL_CATEGORY_MAP = {
+  "0401": "생활화학제품",
+  "0208": "위생용품",
+  "0205": "의약외품",
+  "0405": "생활방사선제품",
+  "0206": "화장품",
+  "0403": "먹는물",
+  "0203": "축산물",
+  "0101": "공산품",
+  "0207": "의료기기",
+  "0204": "의약품",
+  "0201": "식품",
+  "0202": "식품",
+  "0301": "자동차"
+  // '해외 리콜':'0501',
+} as const;
+
 export type RECALL_CATEGORY_KEY_TYPE = keyof typeof RECALL_CATEGORY_KEY_MAP;
+export type RECALL_CATEGORY_TYPE = keyof typeof RECALL_CATEGORY_MAP;
