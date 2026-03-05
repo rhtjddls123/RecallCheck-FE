d4d4716abb9363ebc49efb271230b19c3ab1dc5f
unknown <jjjk0605@naver.com>
2026-03-05 22:02:17 +0900

    chore: vercel analytics 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className={`flex justify-center`}>
         <main className=" relative h-dvh w-93.75">{children}</main>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
