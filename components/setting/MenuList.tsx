ce9255ca105060028f754b0fab451e71234857be
unknown <jjjk0605@naver.com>
2026-03-10 14:53:45 +0900

    feat: 약관 및 정책, 로그아웃 및 회원 탈퇴 시 확인 절차 구현

--- components/setting/MenuList.tsx
+++ components/setting/MenuList.tsx
@@ -4,6 +4,8 @@ import { useLogout } from "@/hooks/useLogout";
 import { Switch } from "../ui/switch";
 import { useAuthStore } from "@/store/authStore";
 import { useDeleteAccount } from "@/hooks/useDeleteAccount";
+import TermsPolicyModal from "./TermsPolicyModal";
+import ConfirmModal from "../common/ConfirmModal";
 
 const MenuList = () => {
   const { user } = useAuthStore();
@@ -13,7 +15,7 @@ const MenuList = () => {
     <div className="flex flex-col gap-4">
       <div className="bg-white select-none flex flex-col justify-center gap-2 text-16_M rounded-xl">
         <div className="p-4 cursor-pointer">알림 관리</div>
-        <div className="p-4 cursor-pointer">약관 및 정책</div>
+        <TermsPolicyModal />
         <div className="p-4 flex items-center justify-between">
           <label htmlFor="dark-mode" className="cursor-pointer">
             다크 모드로 전환
@@ -24,12 +26,24 @@ const MenuList = () => {
 
       {user && (
         <div className="text-14_M text-gray-400 flex justify-center gap-4">
-          <span className="underline cursor-pointer" onClick={onLogout}>
-            로그아웃
-          </span>
-          <span className="underline cursor-pointer" onClick={onDeleteAccount}>
-            회원탈퇴
-          </span>
+          <ConfirmModal title="로그아웃" description="로그아웃 하시겠습니까?" onConfirm={onLogout}>
+            <span className="underline cursor-pointer">로그아웃</span>
+          </ConfirmModal>
+          <ConfirmModal
+            title="회원탈퇴"
+            description={
+              <>
+                <span>정말 탈퇴하시겠습니까?</span>
+                <br />
+                <span>
+                  탈퇴 시 모든 개인정보(검색 기록, 방문 기록)는 즉시 삭제되며 복구할 수 없습니다.
+                </span>
+              </>
+            }
+            onConfirm={onDeleteAccount}
+          >
+            <span className="underline cursor-pointer">회원탈퇴</span>
+          </ConfirmModal>
         </div>
       )}
     </div>
