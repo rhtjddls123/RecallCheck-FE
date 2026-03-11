6f2269b24bd6f9a387b4940b4219f9e90ee4933f
unknown <jjjk0605@naver.com>
2026-03-11 22:30:03 +0900

    feat: 생활방사선제품 카테고리 상세정보 구현

--- components/recall/[recallSn]/RecallProductBody.tsx
+++ components/recall/[recallSn]/RecallProductBody.tsx
@@ -7,8 +7,12 @@ interface RecallProductBodyProps {
 }
 
 const RecallProductBody = ({ recallDetail }: RecallProductBodyProps) => {
-  if (recallDetail.cntntsId === "0101" || recallDetail.cntntsId === "0203")
-    return <IndustrialProductBody {...recallDetail} />; // 공산품, 축산물(항목이 없음)
+  if (
+    recallDetail.cntntsId === "0101" ||
+    recallDetail.cntntsId === "0203" ||
+    recallDetail.cntntsId === "0403"
+  )
+    return <IndustrialProductBody {...recallDetail} />; // 공산품, 축산물(항목이 없음), 먹는물(항목이 없음)
   if (recallDetail.cntntsId === "0301") return <AutomobileProductBody {...recallDetail} />; // 자동차
   if (
     recallDetail.cntntsId === "0204" ||
@@ -18,6 +22,7 @@ const RecallProductBody = ({ recallDetail }: RecallProductBodyProps) => {
     return <MedicineProductBody {...recallDetail} />; //의약품, 의약외품, 화장품
   if (recallDetail.cntntsId === "0207") return <MedicalDeviceProductBody {...recallDetail} />; // 의료기기
   if (recallDetail.cntntsId === "0208") return <HygieneProductBody {...recallDetail} />; // 위생용품
+  if (recallDetail.cntntsId === "0405") return <DailyRadiationProductBody {...recallDetail} />; // 생활방사선제품
 };
 
 const IndustrialProductBody = ({
@@ -249,6 +254,62 @@ const HygieneProductBody = ({
       description: `${recallPublictBgnde || ""} ~ ${recallPublictEndde || ""}`
     },
     { title: "출처", description: infoOriginInstt },
+    { title: "위해원인 및 결과", description: injryCauseResult }
+  ];
+
+  return (
+    <div className="p-4 bg-white flex flex-col gap-5">
+      <BodyBaseSection title="리콜이유">
+        <BodyBaseTable data={tableData} />
+      </BodyBaseSection>
+
+      <BodyBaseSection title="소비자 행동요령">
+        <p className="text-14_M">{cnsmrGhvrTips}</p>
+      </BodyBaseSection>
+
+      <BodyBaseSection title="리콜 기간">
+        <p className="text-14_M">{`${recallBgnde || ""} ~ ${recallEndde || ""}`}</p>
+      </BodyBaseSection>
+
+      <BodyBaseSection title="문의처, 업체주소">
+        <p className="text-14_M whitespace-pre-line">{recallEntrpsInfo}</p>
+      </BodyBaseSection>
+
+      <BodyBaseSection title="정보 제공 기관">
+        <div className="flex flex-col gap-1">
+          <p className="text-14_M">{infoOriginInstt || "-"}</p>
+          {infoOriginInsttUrl && (
+            <Link href={infoOriginInsttUrl} target="_blank" className="text-14_M underline">
+              {infoOriginInsttUrl}
+            </Link>
+          )}
+        </div>
+      </BodyBaseSection>
+    </div>
+  );
+};
+
+const DailyRadiationProductBody = ({
+  recallSe,
+  injryCauseResult,
+  recallPublictBgnde,
+  recallPublictEndde,
+  infoOriginInstt,
+  recallBgnde,
+  recallEndde,
+  recallEntrpsInfo,
+  infoOriginInsttUrl,
+  cnsmrGhvrTips,
+  shrtcomCn
+}: RecallType) => {
+  const tableData: { title: string; description: string | null | string[] }[] = [
+    { title: "리콜구분", description: recallSe },
+    {
+      title: "리콜 공표기간",
+      description: `${recallPublictBgnde || ""} ~ ${recallPublictEndde || ""}`
+    },
+    { title: "출처", description: infoOriginInstt },
+    { title: "결함의 내용", description: shrtcomCn },
     { title: "위해원인 및 결과", description: injryCauseResult }
   ];
 
--- components/recall/[recallSn]/RecallProductHeader.tsx
+++ components/recall/[recallSn]/RecallProductHeader.tsx
@@ -14,17 +14,22 @@ interface RecallProductHeaderProps {
 }
 
 const RecallProductHeader = ({ recallDetail }: RecallProductHeaderProps) => {
-  if (recallDetail.cntntsId === "0101" || recallDetail.cntntsId === "0203")
-    return <IndustrialProductHeader {...recallDetail} />; // 공산품, 축산물(항목이 없음)
+  if (
+    recallDetail.cntntsId === "0101" ||
+    recallDetail.cntntsId === "0203" ||
+    recallDetail.cntntsId === "0403"
+  )
+    return <IndustrialProductHeader {...recallDetail} />; // 공산품, 축산물(항목이 없음), 먹는물(항목이 없음)
   if (recallDetail.cntntsId === "0301") return <AutomobileProductHeader {...recallDetail} />; // 자동차
   if (
     recallDetail.cntntsId === "0204" ||
     recallDetail.cntntsId === "0205" ||
     recallDetail.cntntsId === "0206"
   )
-    return <MedicineProductHeader {...recallDetail} />; // 의약품, 의약외품
+    return <MedicineProductHeader {...recallDetail} />; // 의약품, 의약외품, 화장품
   if (recallDetail.cntntsId === "0207") return <MedicalDeviceProductHeader {...recallDetail} />; // 의료기기
   if (recallDetail.cntntsId === "0208") return <HygieneProductHeader {...recallDetail} />; // 위생용품
+  if (recallDetail.cntntsId === "0405") return <DailyRadiationProductHeader {...recallDetail} />; // 생활방사선제품
   return;
 };
 
@@ -167,6 +172,58 @@ const HygieneProductHeader = ({
     { title: "제조사", description: makr },
     { title: "제조연월일", description: mnfcturPd },
     { title: "모델명", description: modlNmInfo }
+  ];
+
+  return (
+    <div className="p-4 flex flex-col gap-5 bg-white">
+      <Carousel>
+        <CarouselContent>
+          {recallImgUrls && recallImgUrls.length > 0 ? (
+            recallImgUrls.map((item) => (
+              <CarouselItem key={item}>
+                <ImageWithDefault
+                  src={item}
+                  className="rounded-2xl object-cover max-h-85.75 w-85.75"
+                />
+              </CarouselItem>
+            ))
+          ) : (
+            <CarouselItem>
+              <ImageWithDefault className="rounded-2xl object-cover" />
+            </CarouselItem>
+          )}
+        </CarouselContent>
+        {recallImgUrls && recallImgUrls.length > 1 && (
+          <>
+            <CarouselPrevious className="left-2" />
+            <CarouselNext className="right-2" />
+          </>
+        )}
+      </Carousel>
+
+      <div className="flex flex-col gap-5">
+        <h2 className="text-18_B text-gray-800">{productNm}</h2>
+        <HeaderBaseTable data={tableData} />
+      </div>
+    </div>
+  );
+};
+
+const DailyRadiationProductHeader = ({
+  productNm,
+  cntntsId,
+  makr,
+  mnfcturPd,
+  modlNmInfo,
+  mainSleoffic,
+  recallImgUrls
+}: RecallType) => {
+  const tableData: { title: string; description: string | null }[] = [
+    { title: "카테고리", description: RECALL_CATEGORY_MAP[cntntsId] },
+    { title: "제조사", description: makr },
+    { title: "제조연월일", description: mnfcturPd },
+    { title: "모델명", description: modlNmInfo },
+    { title: "주요판매처", description: mainSleoffic }
   ];
 
   return (
