8d3a6bd2fbb24cc8f8a2c650dc72ec7e33ad5c28
unknown <jjjk0605@naver.com>
2026-03-11 20:59:11 +0900

    refactor: 자동차 카테고리 상세정보 구현

--- components/recall/[recallSn]/RecallProductBody.tsx
+++ components/recall/[recallSn]/RecallProductBody.tsx
@@ -1,30 +1,17 @@
 import { RecallType } from "@/types/recall.type";
 import Link from "next/link";
+import { ReactNode } from "react";
 
 interface RecallProductBodyProps {
   recallDetail: RecallType;
 }
 
 const RecallProductBody = ({ recallDetail }: RecallProductBodyProps) => {
-  if (recallDetail.cntntsId === "0101") return <IndustrialProductBody {...recallDetail} />;
+  if (recallDetail.cntntsId === "0101") return <IndustrialProductBody {...recallDetail} />; // 공산품
+  if (recallDetail.cntntsId === "0301") return <AutomobileProductBody {...recallDetail} />; // 자동차
 
   return;
 };
-
-interface IndustrialProductBodyProps {
-  recallSe: string | null; // 리콜 구분
-  recallPublictBgnde: string | null; // 리콜 공표 시작일
-  recallPublictEndde: string | null; // 리콜 공표 만료일
-  injryCauseResult: string | null; // 위해 원인
-
-  cnsmrGhvrTips: string | null; // 소비자 행동 요령
-
-  recallProcssInfo: string | null; // 리콜 절차
-  etcInfo: string | null; // 기타 정보
-
-  infoOriginInstt: string | null; // 정보 출처 기관
-  infoOriginInsttUrl: string | null; // 정보 출처 URL
-}
 
 const IndustrialProductBody = ({
   recallSe,
@@ -36,7 +23,7 @@ const IndustrialProductBody = ({
   etcInfo,
   infoOriginInstt,
   infoOriginInsttUrl
-}: IndustrialProductBodyProps) => {
+}: RecallType) => {
   const injryArray = injryCauseResult
     ?.split(/(?=ㅇ )/)
     .flatMap((s) => s.split(/(?=- )/))
@@ -48,61 +35,37 @@ const IndustrialProductBody = ({
     .map((s) => s.trim())
     .filter(Boolean);
 
+  const tableData: { title: string; description: string | null | string[] }[] = [
+    { title: "리콜구분", description: recallSe },
+    {
+      title: "리콜 공표기간",
+      description: `${recallPublictBgnde || ""} ~ ${recallPublictEndde || ""}`
+    },
+    { title: "출처", description: infoOriginInstt },
+    { title: "위해원인 및 결과", description: injryArray || [] }
+  ];
+
   return (
     <div className="p-4 bg-white flex flex-col gap-5">
-      <section className="flex flex-col gap-3">
-        <h2 className="text-16_B">리콜이유</h2>
+      <BodyBaseSection title="리콜이유">
+        <BodyBaseTable data={tableData} />
+      </BodyBaseSection>
 
-        <table className="w-full border-collapse">
-          <tbody>
-            <tr className="border-t border-gray-200">
-              <td className="py-2 pr-4 text-gray-700 whitespace-nowrap w-32 text-14_B">리콜구분</td>
-              <td className="py-2 text-gray-700 text-12_M">{recallSe || "-"}</td>
-            </tr>
-            <tr className="border-t border-gray-200">
-              <td className="py-2 pr-4 text-gray-700 whitespace-nowrap w-32 text-14_B">
-                리콜 공표기간
-              </td>
-              <td className="py-2 text-gray-700 text-12_M">{`${recallPublictBgnde || ""} ~ ${recallPublictEndde || ""}`}</td>
-            </tr>
-            <tr className="border-t border-gray-200">
-              <td className="py-2 pr-4 text-gray-700 whitespace-nowrap w-32 text-14_B">출처</td>
-              <td className="py-2 text-gray-700 text-12_M">{infoOriginInstt || "-"}</td>
-            </tr>
-            <tr className="border-t border-gray-200">
-              <td className="py-2 pr-4 text-gray-700 whitespace-nowrap w-32 text-14_B">
-                위해원인 및 결과
-              </td>
-
-              <td className="py-2 text-gray-700 text-12_M flex flex-col gap-1">
-                {injryArray && injryArray.length > 0
-                  ? injryArray.map((v) => <p key={v}>{v}</p>)
-                  : "-"}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
-
-      <section className="flex flex-col gap-3">
-        <h2 className="text-16_B">소비자 행동요령</h2>
+      <BodyBaseSection title="소비자 행동요령">
         <div className="text-14_M flex flex-col gap-1">
           {tipsArray && tipsArray.length > 0 ? tipsArray.map((v) => <p key={v}>{v}</p>) : "-"}
         </div>
-      </section>
+      </BodyBaseSection>
 
-      <section className="flex flex-col gap-3">
-        <h2 className="text-16_B">리콜 절차</h2>
+      <BodyBaseSection title="리콜 절차">
         <p className="text-14_M">{recallProcssInfo || "-"}</p>
-      </section>
+      </BodyBaseSection>
 
-      <section className="flex flex-col gap-3">
-        <h2 className="text-16_B">기타 정보</h2>
+      <BodyBaseSection title="기타 정보">
         <p className="text-14_M">{etcInfo || "-"}</p>
-      </section>
+      </BodyBaseSection>
 
-      <section className="flex flex-col gap-3">
-        <h2 className="text-16_B">정보 제공 기관</h2>
+      <BodyBaseSection title="정보 제공 기관">
         <div className="flex flex-col gap-1">
           <p className="text-14_M">{infoOriginInstt || "-"}</p>
           {infoOriginInsttUrl && (
@@ -111,8 +74,99 @@ const IndustrialProductBody = ({
             </Link>
           )}
         </div>
-      </section>
+      </BodyBaseSection>
     </div>
+  );
+};
+
+const AutomobileProductBody = ({
+  recallSe,
+  recallPublictBgnde,
+  recallPublictEndde,
+  infoOriginInstt,
+  shrtcomCn,
+  recallBgnde,
+  recallEndde,
+  recallEntrpsInfo,
+  etcInfo,
+  infoOriginInsttUrl
+}: RecallType) => {
+  const tableData: { title: string; description: string | null | string[] }[] = [
+    { title: "리콜구분", description: recallSe },
+    {
+      title: "리콜 공표기간",
+      description: `${recallPublictBgnde || ""} ~ ${recallPublictEndde || ""}`
+    },
+    { title: "출처", description: infoOriginInstt },
+    { title: "결함의 내용", description: shrtcomCn }
+  ];
+
+  return (
+    <div className="p-4 bg-white flex flex-col gap-5">
+      <BodyBaseSection title="리콜이유">
+        <BodyBaseTable data={tableData} />
+      </BodyBaseSection>
+
+      <BodyBaseSection title="리콜 기간">
+        <p className="text-14_M">{`${recallBgnde || ""} ~ ${recallEndde || ""}`}</p>
+      </BodyBaseSection>
+      <BodyBaseSection title="문의처, 업체주소">
+        <p className="text-14_M">{recallEntrpsInfo || "-"}</p>
+      </BodyBaseSection>
+      <BodyBaseSection title="기타 정보">
+        <p className="text-14_M" dangerouslySetInnerHTML={{ __html: etcInfo || "-" }} />
+      </BodyBaseSection>
+
+      <BodyBaseSection title="정보 제공 기관">
+        <div className="flex flex-col gap-1">
+          <p className="text-14_M">{infoOriginInstt || "-"}</p>
+          {infoOriginInsttUrl && (
+            <Link href={infoOriginInsttUrl} target="_blank" className="text-14_M underline">
+              {infoOriginInsttUrl}
+            </Link>
+          )}
+        </div>
+      </BodyBaseSection>
+    </div>
+  );
+};
+
+const BodyBaseSection = ({ title, children }: { title: string; children: ReactNode }) => {
+  return (
+    <section className="flex flex-col gap-3">
+      <h2 className="text-16_B">{title}</h2>
+      {children}
+    </section>
+  );
+};
+
+const BodyBaseTable = ({
+  data
+}: {
+  data: { title: string; description: string | null | string[] }[];
+}) => {
+  return (
+    <table className="w-full border-collapse">
+      <tbody>
+        {data.map((v) => (
+          <tr key={v.title} className="border-t border-gray-200">
+            <td className="py-2 pr-4 text-gray-700 whitespace-nowrap w-32 text-14_B">{v.title}</td>
+            {Array.isArray(v.description) ? (
+              <td className="py-2 text-gray-700 text-12_M flex flex-col gap-1">
+                {v.description.length > 0
+                  ? v.description.map((item) => <p key={item}>{item}</p>)
+                  : v.description || "-"}
+              </td>
+            ) : (
+              <td
+                className="py-2 text-gray-700 text-12_M flex flex-col gap-1"
+                dangerouslySetInnerHTML={{ __html: v.description || "-" }}
+              />
+            )}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
--- components/recall/[recallSn]/RecallProductHeader.tsx
+++ components/recall/[recallSn]/RecallProductHeader.tsx
@@ -6,7 +6,7 @@ import {
   CarouselNext,
   CarouselPrevious
 } from "@/components/ui/carousel";
-import { RECALL_CATEGORY_MAP, RECALL_CATEGORY_TYPE } from "@/const/RECALL_CATEGORY_KEY_MAP.const";
+import { RECALL_CATEGORY_MAP } from "@/const/RECALL_CATEGORY_KEY_MAP.const";
 import { RecallType } from "@/types/recall.type";
 
 interface RecallProductHeaderProps {
@@ -14,19 +14,10 @@ interface RecallProductHeaderProps {
 }
 
 const RecallProductHeader = ({ recallDetail }: RecallProductHeaderProps) => {
-  if (recallDetail.cntntsId === "0101") return <IndustrialProductHeader {...recallDetail} />;
-
+  if (recallDetail.cntntsId === "0101") return <IndustrialProductHeader {...recallDetail} />; // 공산품
+  if (recallDetail.cntntsId === "0301") return <AutomobileProductHeader {...recallDetail} />; // 자동차
   return;
 };
-
-interface IndustrialProductHeaderProps {
-  productNm: string; // 제품명
-  cntntsId: string; // 메뉴 ID
-  stdBrcd: string | null; // 표준 바코드(유통표준코드)
-  bsnmNm: string | null; // 사업자명
-  modlNmInfo: string | null; // 모델명 정보
-  recallImgUrls: string[] | null;
-}
 
 const IndustrialProductHeader = ({
   productNm,
@@ -35,7 +26,14 @@ const IndustrialProductHeader = ({
   bsnmNm,
   modlNmInfo,
   recallImgUrls
-}: IndustrialProductHeaderProps) => {
+}: RecallType) => {
+  const tableData: { title: string; description: string | null }[] = [
+    { title: "카테고리", description: RECALL_CATEGORY_MAP[cntntsId] },
+    { title: "유통표준코드", description: stdBrcd },
+    { title: "사업자명", description: bsnmNm },
+    { title: "모델명", description: modlNmInfo }
+  ];
+
   return (
     <div className="p-4 flex flex-col gap-5 bg-white">
       <Carousel>
@@ -65,33 +63,45 @@ const IndustrialProductHeader = ({
 
       <div className="flex flex-col gap-5">
         <h2 className="text-18_B text-gray-800">{productNm}</h2>
-
-        <table className="w-full border-collapse text-sm">
-          <tbody>
-            <tr className="border-t border-gray-200">
-              <td className="py-2 pr-4 text-gray-700 whitespace-nowrap w-32 text-14_B">카테고리</td>
-              <td className="py-2 text-gray-700 text-12_M">
-                {RECALL_CATEGORY_MAP[cntntsId as RECALL_CATEGORY_TYPE]}
-              </td>
-            </tr>
-            <tr className="border-t border-gray-200">
-              <td className="py-2 pr-4 text-gray-700 whitespace-nowrap w-32 text-14_B">
-                유통표준코드
-              </td>
-              <td className="py-2 text-gray-700 text-12_M">{stdBrcd || "-"}</td>
-            </tr>
-            <tr className="border-t border-gray-200">
-              <td className="py-2 pr-4 text-gray-700 whitespace-nowrap w-32 text-14_B">사업자명</td>
-              <td className="py-2 text-gray-700 text-12_M">{bsnmNm || "-"}</td>
-            </tr>
-            <tr className="border-t border-gray-200">
-              <td className="py-2 pr-4 text-gray-700 whitespace-nowrap w-32 text-14_B">모델명</td>
-              <td className="py-2 text-gray-700 text-12_M">{modlNmInfo || "-"}</td>
-            </tr>
-          </tbody>
-        </table>
+        <HeaderBaseTable data={tableData} />
       </div>
     </div>
+  );
+};
+
+const AutomobileProductHeader = ({
+  cntntsId,
+  productNm,
+  bsnmNm,
+  mnfcturPd,
+  modlNmInfo
+}: RecallType) => {
+  const tableData: { title: string; description: string | null }[] = [
+    { title: "카테고리", description: RECALL_CATEGORY_MAP[cntntsId] },
+    { title: "제조사", description: bsnmNm },
+    { title: "제조연월일", description: mnfcturPd },
+    { title: "모델명", description: modlNmInfo }
+  ];
+  return (
+    <div className="p-4 flex flex-col gap-5 bg-white">
+      <h2 className="text-18_B text-gray-800">{productNm}</h2>
+      <HeaderBaseTable data={tableData} />
+    </div>
+  );
+};
+
+const HeaderBaseTable = ({ data }: { data: { title: string; description: string | null }[] }) => {
+  return (
+    <table className="w-full border-collapse">
+      <tbody>
+        {data.map((v) => (
+          <tr key={v.title} className="border-t border-gray-200">
+            <td className="py-2 pr-4 text-gray-700 whitespace-nowrap w-32 text-14_B">{v.title}</td>
+            <td className="py-2 text-gray-700 text-12_M">{v.description}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
