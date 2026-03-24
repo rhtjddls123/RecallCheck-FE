34d0838e53a3ea20856b36e6340fb45a540d1aea
unknown <jjjk0605@naver.com>
2026-03-24 14:36:06 +0900

    refactor: SearchInput 최적화

--- components/common/SearchInput.tsx
+++ components/common/SearchInput.tsx
@@ -3,7 +3,7 @@
 import { FormEvent, useRef, useState } from "react";
 import { cn } from "@/lib/utils";
 import { SearchIcon, XIcon } from "lucide-react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const SearchInput = () => {
   const [focused, setFocused] = useState(false);
@@ -12,14 +12,15 @@ const SearchInput = () => {
   const inputRef = useRef<HTMLInputElement | null>(null);
   const xButtonRef = useRef<HTMLButtonElement | null>(null);
   const router = useRouter();
+  const pathname = usePathname();
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const query = inputRef.current?.value;
-    if (!query) return;
+    const inputValue = inputRef.current?.value;
+    if (!inputValue) return;
 
     const params = new URLSearchParams(searchParams.toString());
-    params.set("query", query);
+    params.set("query", inputValue);
     params.set("page", "1");
 
     router.push(`/recall?${params.toString()}`);
@@ -27,17 +28,20 @@ const SearchInput = () => {
 
   const onDelete = () => {
     if (inputRef.current?.value) inputRef.current.value = "";
-    const params = new URLSearchParams(searchParams.toString());
-    params.delete("query");
-    params.set("page", "1");
+    if (pathname !== "/") {
+      const params = new URLSearchParams(searchParams.toString());
+      params.delete("query");
+      params.set("page", "1");
 
-    router.push(`/recall?${params.toString()}`);
+      router.push(`/recall?${params.toString()}`);
+    }
     inputRef.current?.blur();
     xButtonRef.current?.blur();
   };
 
   return (
     <form
+      key={query ?? ""}
       onSubmit={handleSubmit}
       className="flex w-full items-center bg-gray-100 gap-2 rounded-[100px] bg-gray-25 dark:bg-zinc-700 px-3 py-1.5 focus-within:outline focus-within:outline-gray-700 dark:focus-within:outline-zinc-400"
     >
